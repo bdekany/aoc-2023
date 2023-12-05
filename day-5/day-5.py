478160830431a46afec2d499f75d2a419e87b2fd
Brice DEKANY <brice.dekany@gmail.com>
2023-12-05 15:26:20 +0000

    Day 5 part 2, solved brute force

--- day-5/day-5.py
+++ day-5/day-5.py
@@ -26,6 +26,15 @@ class Jardinier:
         # renvoie dest == source
         return entree
     
+    def trouve_inverse(self, nom_de_map, destination):
+        for map in self.maps[nom_de_map]:
+            if map["dest"] <= destination and destination <= map["dest"] + map["gamme"]:
+                gamme = destination - map["dest"]
+                # renvoi la source
+                return map["source"] + gamme
+        # renvoie dest == source
+        return destination
+
     def trouve_localisation_depuis_seed(self, entree):
         return self.trouve_correspondance("humidity-to-location",
                 self.trouve_correspondance("temperature-to-humidity",
@@ -34,10 +43,20 @@ class Jardinier:
                 self.trouve_correspondance("fertilizer-to-water",
                 self.trouve_correspondance("soil-to-fertilizer",
                 self.trouve_correspondance("seed-to-soil", entree)))))))
+    
+    def trouve_seed_depuis_localisation(self, entree):
+        return self.trouve_inverse("seed-to-soil",
+                self.trouve_inverse("soil-to-fertilizer",
+                self.trouve_inverse("fertilizer-to-water",
+                self.trouve_inverse("water-to-light",
+                self.trouve_inverse("light-to-temperature",
+                self.trouve_inverse("temperature-to-humidity",
+                self.trouve_inverse("humidity-to-location", entree)))))))
 
 
 resulat_puzzle = 0
 liste_seeds = []
+liste_seeds_part2 = []
 nom_de_map = ""
 gardener = Jardinier()
 
@@ -50,6 +69,8 @@ with open(nom_fichier, 'r') as fichier:
         if ligne.startswith("seeds: "):
             # convertion en Int de la ligne seeds: 1 2 3 4
             liste_seeds = list(map(lambda x: int(x), ligne.split()[1:]))
+            for i in range(0, len(liste_seeds), 2):
+                liste_seeds_part2.append({"seed": liste_seeds[i], "gamme": liste_seeds[i+1]-1})
             continue
         
         # Detection debut map
@@ -62,7 +83,7 @@ with open(nom_fichier, 'r') as fichier:
             dest, source, gamme = map(lambda x: int(x), ligne.split())
             gardener.maps[nom_de_map].append({'dest': dest, 'source': source, 'gamme': gamme-1})
 
-## Test
+## Test Part 1
 # seed = 4106085912
 # print(gardener.trouve_localisation_depuis_seed(seed))
 
@@ -71,5 +92,18 @@ for seed in liste_seeds:
     if resulat_puzzle > localisation or resulat_puzzle == 0:
         resulat_puzzle = localisation
 
+## part 2
+stop = 1
+#for seed in liste_seeds_part2:
+# ???? Mouarf
+for loc in range(103000000,141000000):
+    if stop:
+        seed_potentiel = gardener.trouve_seed_depuis_localisation(loc)
+        for almanac_seed in liste_seeds_part2:
+            if almanac_seed["seed"] <= seed_potentiel and seed_potentiel <= almanac_seed["seed"] + almanac_seed["gamme"]:
+                print("Part2 Loc", loc, "seed", seed_potentiel)
+                stop = 0
+
 print("===== Réponse du day-1 =====")
 print("Part 1:", resulat_puzzle)
+print("Part 2:", loc)
